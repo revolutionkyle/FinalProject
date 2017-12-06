41724f0e94a233647e8db67642acf9429cf19479
revolutionkyle <khammer4@mail.csuchico.edu>
2017-12-05 21:26:44 -0800

    Made the cipher more consistent between 0 and 26.

--- encrypt.cpp
+++ encrypt.cpp
@@ -5,7 +5,7 @@ std::vector<char> encrypt::encryptword(std::string word, int a)
 { std::vector<char> encrypt;
   for(int i=0;i<word.size();i++)
   {
-    encrypt.push_back(char(((int(word[i])+a)% 26) +97));
+    encrypt.push_back(char(((int(word[i])+7+a)% 26) +97));
   }
   return encrypt;
 }
